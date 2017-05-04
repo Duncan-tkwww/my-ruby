217d5e886d586af212b1212c2e6bebfbdde3c59f
DunCan <duxiao@xogrp.com>
2017-05-05 00:29:33 +0800

    Create article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501141648) do
+ActiveRecord::Schema.define(version: 20170504155206) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "uses", force: :cascade do |t|
     t.string   "first_name"
